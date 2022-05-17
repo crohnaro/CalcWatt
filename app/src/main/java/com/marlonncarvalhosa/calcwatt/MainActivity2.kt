9e33e87575fb08f28b9d5dd65471cb9eecf9d596
crohnaro <viniciosalfa@hotmail.com>
2022-05-17 09:54:20 -0300

    Iniciando spinner com estados

--- app/src/main/java/com/marlonncarvalhosa/calcwatt/MainActivity2.kt
+++ app/src/main/java/com/marlonncarvalhosa/calcwatt/MainActivity2.kt
@@ -2,10 +2,37 @@ package com.marlonncarvalhosa.calcwatt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity2 : AppCompatActivity() {
+
+    lateinit var option : Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+       option = findViewById(R.id.cidadesescolha) as Spinner
+
+       val options = arrayOf("RJ", "SP")
+       option.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options)
+
+        option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
     }
 }
