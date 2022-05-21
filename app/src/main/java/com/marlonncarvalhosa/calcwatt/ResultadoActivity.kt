b010a11d51b7e0ff57d8d1a73bd90d3c368fd79a
crohnaro <viniciosalfa@hotmail.com>
2022-05-21 11:18:37 -0300

    adicionando finishactivity na ultima activity

--- app/src/main/java/com/marlonncarvalhosa/calcwatt/ResultadoActivity.kt
+++ app/src/main/java/com/marlonncarvalhosa/calcwatt/ResultadoActivity.kt
@@ -1,11 +1,23 @@
 package com.marlonncarvalhosa.calcwatt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class ResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        val btn = findViewById<Button>(R.id.novaconsulta)
+        btn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(applicationContext, CalculoAcitivity::class.java)
+            startActivity(intent)
+            finish()
+        })
+    
     }
+
 }
