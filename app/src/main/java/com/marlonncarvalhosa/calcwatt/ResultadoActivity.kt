4c8176099ce048d8f44d21d54dcc4b945adccba7
crohnaro <viniciosalfa@hotmail.com>
2022-05-22 22:06:42 -0300

    Subindo novo metodo de fazer os o calculo

--- app/src/main/java/com/marlonncarvalhosa/calcwatt/ResultadoActivity.kt
+++ app/src/main/java/com/marlonncarvalhosa/calcwatt/ResultadoActivity.kt
@@ -5,19 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class ResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 
-        val btn = findViewById<Button>(R.id.novaconsulta)
-        btn.setOnClickListener(View.OnClickListener {
-            val intent = Intent(applicationContext, CalculoAcitivity::class.java)
-            startActivity(intent)
-            finish()
-        })
-    
+        val BandeiraVerde = intent.getStringExtra("Bandeira Verde")
+        val BandeiraVerdeTextView = findViewById<TextView>(R.id.BandeiraVerde).apply {
+            text = BandeiraVerde
+        }
+        val BandeiraVermelha = intent.getStringExtra("Bandeira Vermelha")
+        val BandeiraVermelhaTextView = findViewById<TextView>(R.id.BandeiraVermelha).apply {
+            text = BandeiraVermelha
+        }
+        val BandeiraAmarela = intent.getStringExtra("Bandeira Amarela")
+        val BandeiraAmarelaTextView = findViewById<TextView>(R.id.BandeiraAmarela).apply {
+            text = BandeiraAmarela
+        }
     }
 
 }
